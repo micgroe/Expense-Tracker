07ef388de585a65532bbbd4be9fa914a24358cd7
Michael Gröchenig <micgroe@gmail.com>
2024-01-10 00:56:37 +0100

    added MongoDB packages, first test with Expense Object schemas

---
+++ ExpenseTracker/TestManager.swift
@@ -0,0 +1,8 @@
+//
+//  TestManager.swift
+//  ExpenseTracker
+//
+//  Created by Michael Gröchenig on 23.12.23.
+//
+
+import Foundation
---
+++ Objects/ExpenseObject.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseObject.swift
+//  ExpenseTracker
+//
+//  Created by Michael Gröchenig on 19.12.23.
+//
+
+import Foundation
